50021cc6bcef1df9a2cacb15eed18978dfcbfbe1
trandinhthang203 <tranthangkhuong203@gmail.com>
2024-05-20 11:36:17 +0700

    lol

---
+++ app/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name="home"),
+    path('cart/', views.cart, name="cart"),
+    path('checkout/', views.checkout, name="checkout"),
+    path('update_item/', views.updateItem, name="update_item"),
+    path('detail/', views.detail, name="detail"),
+
+]
